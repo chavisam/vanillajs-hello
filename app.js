7425d0d9839ede1035ec0783644a01cb0a86f0e2
Javier <jmanteca@jmanvel.com>
2021-09-20 14:48:00 +0000

    varias_extensiones

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let pronoun = ['el','la'];
 let adj = ['gran', 'enorme' ];
 let noun = ['web','app'];
+let extension = ['.com','.es','.net','.org']
 
 var domains = document.getElementById('result');
 
@@ -11,12 +12,19 @@ for ( let word in pronoun) {
         for (let y = 0; y < noun.length; y++) {
             const element = noun[y];
             
+            for (let t = 0; t < extension.length; t++) {
+                const element = extension[t];
+                
+                document.write(pronoun[word]);
+                document.write(adj[i]);
+                document.write(noun[y]);
+                document.write(extension[t]);
+    
+                 document.write('<br>');
 
-            document.write(pronoun[word]);
-            document.write(adj[i]);
-            document.write(noun[y] + '.com');
+            }
 
-             document.write('<br>');
+           
 
 
         }
