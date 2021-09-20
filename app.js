754bce1ae7f597c2fb8d8dcb7275b620462c2376
Javier <jmanteca@jmanvel.com>
2021-09-20 14:36:24 +0000

    primera_parte_terminada

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 let pronoun = ['el','la'];
 let adj = ['gran', 'enorme' ];
 let noun = ['web','app'];
+
+var domains = document.getElementById('result');
+
+for ( let word in pronoun) {
+    
+    for(let i=0 ; i<adj.length ; i++){
+
+        for (let y = 0; y < noun.length; y++) {
+            const element = noun[y];
+            
+
+            document.write(pronoun[word]);
+            document.write(adj[i]);
+            document.write(noun[y]);
+
+             document.write('<br>');
+
+
+        }
+
+    }
+    
+ 
+}
+
